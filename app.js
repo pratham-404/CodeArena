bb92c46344305ad5620b5986d85ab676f16c57a2
Pratham Loya <prathamloya12345@gmail.com>
2023-04-16 14:11:46 +0530

    route of made local

--- app.js
+++ app.js
@@ -9,64 +9,63 @@ const compiler = require("compilex")
 const options = {stats: true}
 compiler.init(options)
 
-app.use('/codemirror-5.65.12', express.static("C:/Users/Pratham/Desktop/ProjectX/codemirror-5.65.12"))
+app.use('/codemirror-5.65.12', express.static("./codemirror-5.65.12"))
 
 app.get("/", function(req, res){
-res.sendFile("C:/Users/Pratham/Desktop/ProjectX/temp.html");
+  res.sendFile("./temp.html", { root: '.' });
 })
 
 app.post("/compile", function(req, res){
+  var code = req.body.code
+  var input = req.body.input
+  var lang = req.body.lang
+  var osys = req.body.os
 
+  try {
     // free temp dir 
     compiler.flush(function(){
-        console.log("Files Deleted")
+      console.log("Files Deleted")
     });
 
-    var code = req.body.code
-    var input = req.body.input
-    var lang = req.body.lang
-    var osys = req.body.os
-
-    try {
-        function handleCompilerOutput(data) {
-            if (data.output) {
-              res.send(data);
-            } else {
-              res.send({ output: "error" });
-            }
-          }
-        
-        switch (lang) {
-          case "c":
-          case "c++":
-            var command = (osys == "windows") ? "g++" : "gcc";
-            if (input) {
-                compiler.compileCPPWithInput({ OS: osys, cmd: command, options: { timeout: 10000 } }, code, input, handleCompilerOutput);
-            } else {
-                compiler.compileCPP({ OS: osys, cmd: command, options: { timeout: 10000 } }, code, handleCompilerOutput);
-            }
-            break;
-          case "java":
-            if (input) {
-              compiler.compileJavaWithInput({ OS: osys }, code, input, handleCompilerOutput);
-            } else {
-              compiler.compileJava({ OS: osys }, code, handleCompilerOutput);
-            }
-            break;
-          case "python":
-            if (input) {
-              compiler.compilePythonWithInput({ OS: osys }, code, input, handleCompilerOutput);
-            } else {
-              compiler.compilePython({ OS: osys }, code, handleCompilerOutput);
-            }
-            break;
-          default:
-            res.send({ output: "Invalid language" });
+    function handleCompilerOutput(data) {
+        if (data.output) {
+          res.send(data);
+        } else {
+          res.send({ output: "error" });
         }
-      } catch (e) {
-        console.log("error", e);
-      }
+    }
       
-})
+    switch (lang) {
+      case "c":
+      case "c++":
+        var command = (osys == "windows") ? "g++" : "gcc";
+        if (input) {
+            compiler.compileCPPWithInput({ OS: osys, cmd: command, options: { timeout: 10000 } }, code, input, handleCompilerOutput);
+        } else {
+            compiler.compileCPP({ OS: osys, cmd: command, options: { timeout: 10000 } }, code, handleCompilerOutput);
+        }
+        break;
+      case "java":
+        if (input) {
+          compiler.compileJavaWithInput({ OS: osys }, code, input, handleCompilerOutput);
+        } else {
+          compiler.compileJava({ OS: osys }, code, handleCompilerOutput);
+        }
+        break;  
+      case "python":
+        if (input) {
+          compiler.compilePythonWithInput({ OS: osys }, code, input, handleCompilerOutput);
+        } else {
+          compiler.compilePython({ OS: osys }, code, handleCompilerOutput);
+        }
+        break;
+      default:
+        res.send({ output: "Invalid language" });
+    }
+  } 
+  catch (e) {
+    console.log("error", e);
+  }
+});
 
 app.listen(8000)
